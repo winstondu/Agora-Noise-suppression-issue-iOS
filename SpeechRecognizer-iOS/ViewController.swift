7bf8950955e549618e05ee13d463187f09f4a26c
Winston Du <wenhaodu694@gmail.com>
2020-09-24 20:46:37 -0700

    Add logs

--- SpeechRecognizer-iOS/ViewController.swift
+++ SpeechRecognizer-iOS/ViewController.swift
@@ -17,6 +17,36 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+        setupNotifications()
+    }
+    
+    func setupNotifications() {
+        // Get the default notification center instance.
+        let nc = NotificationCenter.default
+        nc.addObserver(self,
+                       selector: #selector(handleRouteChange),
+                       name: AVAudioSession.routeChangeNotification,
+                       object: nil)
+    }
+
+    @objc func handleRouteChange(notification: Notification) {
+        guard let routeChangeReason = AVAudioSession.RouteChangeReason(rawValue: notification.userInfo?[AVAudioSessionRouteChangeReasonKey] as! UInt) else {
+            print("AudioSession route change notification not found?")
+            return
+        }
+        let previousRoute = notification.userInfo?[AVAudioSessionRouteChangePreviousRouteKey] as? AVAudioSessionRouteDescription
+        if routeChangeReason == AVAudioSession.RouteChangeReason.newDeviceAvailable {
+            print("AudioSession New audio device available \(notification.debugDescription)")
+        } else if routeChangeReason == AVAudioSession.RouteChangeReason.routeConfigurationChange {
+            print("AudioSession New route config change \(notification.debugDescription)")
+        } else if notification.userInfo?[AVAudioSessionRouteChangeReasonKey] as? UInt == AVAudioSession.RouteChangeReason.categoryChange.rawValue {
+            print("AudioSession category changed to:\n\(AVAudioSession.sharedInstance().currentCategoryDescription) \(notification.debugDescription)")
+        }
+        else if let other = notification.userInfo?[AVAudioSessionRouteChangeReasonKey] as? UInt {
+            print("AudioSession route change notification other: \(other)")
+        } else {
+            print("AudioSession no notification for route change.")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -43,3 +73,8 @@ class ViewController: UIViewController {
     }
 }
 
+extension AVAudioSession {
+    var currentCategoryDescription: String {
+        return "Category: \(self.category),\nMode: \(self.mode), \nCategoryOptions: \(self.categoryOptions)"
+    }
+}
